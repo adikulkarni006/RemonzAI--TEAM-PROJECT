19509b54c76057dbaf8baa73bf0b03107994b9da
Advait Kulkarni <advaitkulkarni006@gmail.com>
2025-08-12 14:21:36 +0530

    update files and folders

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/testDB');
+mongoose.connect('mongodb://localhost:27017/remonzai');
 
 const userSchema = mongoose.Schema({
     email: String,
